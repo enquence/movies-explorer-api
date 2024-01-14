b7750b541092cc325fece2aefbb1a5fcfb61b4d0
Andrey Ognev <enquence@yandex.ru>
2024-01-14 16:33:24 +0300

    feat: signout route added

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 const router = require('express').Router();
 const { celebrate } = require('celebrate');
 const auth = require('../middlewares/auth');
-const { login, createUser } = require('../controllers/users');
+const { login, logout, createUser } = require('../controllers/users');
 const { NotFoundError } = require('../utils/errors');
 const { requestRules } = require('../utils/req-rules');
 const { requestMessages } = require('../utils/vocabulary');
 
-router.post('/signin', celebrate(requestRules.userSignInBody), login);
 router.post('/signup', celebrate(requestRules.userSignUpBody), createUser);
+router.post('/signin', celebrate(requestRules.userSignInBody), login);
+router.post('/signout', auth, logout);
 
 router.use('/users', auth, require('./users'));
 router.use('/movies', auth, require('./movies'));
